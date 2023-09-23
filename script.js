eafa9b12750af85d2663d27572190bbb30143191
Jathusan Uthayakumaran <juthayakumara21@hotmail.co.uk>
2023-09-23 23:34:35 +0100

    Add darkening effect functionality

--- script.js
+++ script.js
@@ -11,8 +11,11 @@ let colorModeOn = true;
 let rainbowModeOn = false;
 let eraser = false;
 
+let gridColorArr = [];
+
 const createGrids = (numOfGrids) => {
   let grid;
+  let gridNum = 0;
 
   for (let i = 1; i <= numOfGrids; i++) {
     row = document.createElement("div");
@@ -22,12 +25,89 @@ const createGrids = (numOfGrids) => {
     for (let j = 1; j <= numOfGrids; j++) {
       grid = document.createElement("div");
       grid.classList.add("grid");
+      grid.setAttribute("id", `${gridNum}`);
 
-      grid.style.color = "white";
-
+      gridNum += 1;
+      gridColorArr.push("");
       row.appendChild(grid);
     }
   }
+};
+
+const rgbFormat = (grid) => {
+  console.log(grid.style.backgroundColor);
+  let rgbColor = grid.style.backgroundColor;
+  let rgbArr = grid.style.backgroundColor
+    .substring(4, rgbColor.length - 1)
+    .replace(/ /g, "")
+    .split(",");
+  console.log(rgbArr);
+
+  let R = Number(rgbArr[0]) / 10;
+  let G = Number(rgbArr[1] / 10);
+  let B = Number(rgbArr[2] / 10);
+
+  let countIteration = 0;
+
+  return `rgb(${R}, ${G}, ${B}, ${countIteration})`;
+};
+
+const darkMode = (grid) => {
+  // console.log(grid.id);
+  let rgbColor = grid.style.backgroundColor;
+  let rgbArr = grid.style.backgroundColor
+    .substring(4, rgbColor.length - 1)
+    .replace(/ /g, "")
+    .split(",");
+  //console.log(rgbArr);
+
+  let R = Number(rgbArr[0]);
+  let G = Number(rgbArr[1]);
+  let B = Number(rgbArr[2]);
+  // console.log(`R:${R}`);
+  // console.log(`G:${G}`);
+  // console.log(`B:${B}`);
+
+  let divideColor = gridColorArr[Number(grid.id)];
+  let divideColorArray = gridColorArr[Number(grid.id)]
+    .substring(4, divideColor.length - 1)
+    .replace(/ /g, "")
+    .split(",");
+
+  let divideColorByR = Number(divideColorArray[0]);
+  let divideColorByG = Number(divideColorArray[1]);
+  let divideColorByB = Number(divideColorArray[2]);
+
+  let countIteration = Number(divideColorArray[3]);
+
+  countIteration += 1;
+
+  if (countIteration <= 9) {
+    if (R >= 0) {
+      //  console.log("R divide by: " + R);
+      R -= divideColorByR;
+    }
+
+    if (G >= 0) {
+      //  console.log("G divide by: " + G);
+      G -= divideColorByG;
+    }
+
+    if (B >= 0) {
+      // console.log("B divide by: " + B);
+      B -= divideColorByB;
+    }
+  } else if (countIteration === 10) {
+    R = 0;
+    G = 0;
+    B = 0;
+  }
+
+  gridColorArr[
+    Number(grid.id)
+  ] = `rgb(${divideColorByR}, ${divideColorByG}, ${divideColorByB}, ${countIteration})`;
+  console.log(`rgb(${R}, ${G}, ${B})`);
+  return `rgb(${R}, ${G}, ${B})`;
 };
 
 const removeGrids = () => {
@@ -56,24 +136,31 @@ const applyColor = (event) => {
   if (grid.style.background === "" && rainbowModeOn != true && colorModeOn) {
     console.log("add new color");
     grid.style.background = colorPicker.value;
+    gridColorArr[Number(grid.id)] = rgbFormat(grid);
+    console.log(gridColorArr[Number(grid.id)]);
   } else if (
     rainbowModeOn &&
     grid.style.background === "" &&
     colorModeOn != true
   ) {
     console.log("add rainbow color");
+
     grid.style.background = generateRainbowColor();
+    gridColorArr[Number(grid.id)] = rgbFormat(grid);
   } else if (eraser && grid.style.background != "") {
     grid.style.background = "";
+  } else {
+    grid.style.backgroundColor = darkMode(grid);
   }
 };
 
 createGrids(5);
 
 slider.addEventListener("input", () => {
+  removeGrids();
+  gridColorArr.length = 0;
   const sliderValue = slider.value;
   sliderValueDisplay.textContent = `${sliderValue} x ${sliderValue}`;
-  removeGrids();
   createGrids(sliderValue);
 });
 
